27ba6bf33398d2e719e3967785b8700a02476791
safyro <vektorn1212@gmail.com>
2024-07-23 23:12:32 +0300

    register product model in django admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+# Register Product model in Django admin
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image_s', 'image_m', 'image_l', 'price',)
+    search_fields = ('name', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
