0c888a456b1951dc01a54b0af7b918810e238e58
safyro <vektorn1212@gmail.com>
2024-07-23 23:12:20 +0300

    register category and subcategory models in django admin

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category, SubCategory
+
+
+# Register Category model in Django admin
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image',)
+    search_fields = ('name', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+# Register SubCategory model in Django admin
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image',)
+    search_fields = ('name', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
